76b42ca5917d193e406c3984b4d5dd2c576b6a0f
Chris Sarmiento <chrismac@Chriss-MacBook-Pro.local>
2022-02-28 00:22:04 -0500

    adding initial fetch requestto js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var apiKey = "0d0839b84e0e9b172f262c1df9ccb1a9"
+
+var getCity= function(cityName){
+    var cityApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=" + apiKey;
+
+    fetch(cityApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        } else {
+            alert("this didnt work!")
+        }
+    });
+}
+
+//testing to see output of console log of the fetch response
+getCity("Boston");
