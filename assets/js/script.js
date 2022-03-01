24d4a963c445d0b73cbed1f5f1ec8c1235af8e32
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-02-28 23:41:06 -0500

    added current weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var apiKey = "0d0839b84e0e9b172f262c1df9ccb1a9";
 
 var inputCityEl=  document.querySelector("#search");
 var searchFormEl = document.querySelector("#search-form");
+var currentWeatherEl = document.querySelector("#current-weather");
+var currentWeatherCityEl =document.querySelector("#current-weather-cityname");
+var currentWeatherInfoEl = document.querySelector("#current-weather-info");
 
 
 // function to user input from search form
@@ -33,6 +36,7 @@ var getCity = function (cityName) {
 
                 var cityLat = data.coord.lat;
                 var cityLon =  data.coord.lon;
+                var nameOfInputCity= data.name;
                 //console.log(cityLat);
                 //console.log(cityLon);
 
@@ -44,6 +48,69 @@ var getCity = function (cityName) {
                      })
                      .then(function(data) {
                          console.log(data);
+                         //console.log(nameOfInputCity);
+
+                         // get current weather info and add to DOM
+                        var currentCityEl = document.createElement("p");
+                        var currentDate = moment.unix(data.current.dt).format("MM/DD/YYYY");
+                        var currentIcon = data.current.weather[0].icon;
+                        var iconImgEl = document.createElement("img");
+                        iconImgEl.setAttribute("src", "http://openweathermap.org/img/wn/"+ currentIcon + "@2x.png");
+                        iconImgEl.setAttribute("alt", data.current.weather[0].description);
+                        //current city and date
+                        currentCityEl.textContent= nameOfInputCity + " " + "(" + currentDate +")"
+                        currentCityEl.className = ("fw-bold fs-2")
+
+                        // current city weather info- temp, wind, humidity, uv 
+                        //temp
+                        var tempEl = document.createElement("p");
+                        var currentTempInfo = data.current.temp;
+                        tempEl.innerHTML= "Temp: " + currentTempInfo + "&#8457";
+
+                        //wind
+                        var windEl = document.createElement("p");
+                        var currentWindInfo = data.current.wind_speed;
+                        windEl.innerHTML = "Wind: " + currentWindInfo + " MPH";
+
+                        // humidity
+                        var humidEl = document.createElement("p");
+                        var currentHumidInfo = data.current.humidity;
+                        humidEl.innerHTML = "Humidity: " + currentHumidInfo + " %"
+
+                        //uv
+                        var uvEl = document.createElement("p");
+                        var currentUvInfo = data.current.uvi;
+                        uvEl.innerHTML = "UV Index: " + currentUvInfo;
+                        // background for uv index depending on condition strength
+                        if (currentUvInfo <= 2) {
+                                           
+                        } else if (currentUvInfo >= 3 && currentUvInfo <=5) {
+                            
+                        } else {
+                            
+                        }
+
+
+                        //append items 
+                        currentWeatherCityEl.appendChild(currentCityEl);
+                        currentWeatherCityEl.appendChild(iconImgEl);
+
+                        currentWeatherInfoEl.appendChild(tempEl);
+                        currentWeatherInfoEl.appendChild(windEl);
+                        currentWeatherInfoEl.appendChild(humidEl);
+                        currentWeatherInfoEl.appendChild(uvEl);
+
+
+                    
+
+
+
+                        
+
+
+
+
+
                      })
 
             });
@@ -52,8 +119,16 @@ var getCity = function (cityName) {
         }
     });
     
-    
-
 }    
+
+    //city name, date, weather icon
+    // temp
+    //wind
+    // humidity
+    // uv index with color for severity
+
+
+
+
 // event listener that searches for city weather data based on user input
 searchFormEl.addEventListener("submit", formSubmitHandler)
