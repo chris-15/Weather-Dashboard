ed01a20f956fa6fce036d9089b15a95511c63324
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-01 15:12:39 -0500

    refactored html in order to condense js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ var apiKey = "0d0839b84e0e9b172f262c1df9ccb1a9";
 var inputCityEl=  document.querySelector("#search");
 var searchFormEl = document.querySelector("#search-form");
 var currentWeatherEl = document.querySelector("#current-weather");
-var currentWeatherCityEl =document.querySelector("#current-weather-cityname");
+var currentWeatherCityEl =document.querySelector("#current-weathercityname");
+var currentCityTitleEl = document.querySelector("#current-city-title");
 var currentWeatherInfoEl = document.querySelector("#current-weather-info");
 
 
@@ -51,58 +52,46 @@ var getCity = function (cityName) {
                          //console.log(nameOfInputCity);
 
                          // get current weather info and add to DOM
-                        var currentCityEl = document.createElement("p");
                         var currentDate = moment.unix(data.current.dt).format("MM/DD/YYYY");
                         var currentIcon = data.current.weather[0].icon;
                         var iconImgEl = document.createElement("img");
                         iconImgEl.setAttribute("src", "http://openweathermap.org/img/wn/"+ currentIcon + "@2x.png");
                         iconImgEl.setAttribute("alt", data.current.weather[0].description);
                         //current city and date
-                        currentCityEl.textContent= nameOfInputCity + " " + "(" + currentDate +")"
-                        currentCityEl.className = ("fw-bold fs-2")
+                        currentCityTitleEl.textContent= nameOfInputCity + " " + "(" + currentDate + ")";
+
+                        currentCityTitleEl.appendChild(iconImgEl);
 
                         // current city weather info- temp, wind, humidity, uv 
+
                         //temp
-                        var tempEl = document.createElement("p");
-                        
+                        var tempSpanEl = document.querySelector("#temp-span");
                         var currentTempInfo = data.current.temp;
-                        tempEl.innerHTML= "Temp: " + currentTempInfo + "&#8457";
+                        tempSpanEl.innerHTML= + currentTempInfo + "&#8457";
 
                         //wind
-                        var windEl = document.createElement("p");
+                        var windSpanEl = document.querySelector("#wind-span");
                         var currentWindInfo = data.current.wind_speed;
-                        windEl.innerHTML = "Wind: " + currentWindInfo + " MPH";
+                        windSpanEl.innerHTML = + currentWindInfo + " MPH";
 
                         // humidity
-                        var humidEl = document.createElement("p");
+                        var humidSpanEl = document.querySelector("#humidity-span");
                         var currentHumidInfo = data.current.humidity;
-                        humidEl.innerHTML = "Humidity: " + currentHumidInfo + " %"
+                        humidSpanEl.innerHTML = + currentHumidInfo + " %"
 
                         //uv
-                        var uvEl = document.createElement("p");
+                        var uvSpanEl = document.querySelector("#uv-span");
                         var currentUvInfo = data.current.uvi;
-                        var spanUvEl= document.createElement("span")
-                        spanUvEl.textContent=currentUvInfo; 
-                        uvEl.innerHTML = "UV Index: ";
-                        uvEl.appendChild(spanUvEl);
+                        uvSpanEl.innerHTML = + currentUvInfo;
+
                         // background for uv index depending on condition strength
                         if (currentUvInfo <= 2) {
-                            spanUvEl.className = ("p-2 bg-success")
+                            uvSpanEl.className = ("p-2 bg-success rounded")
                         } else if (currentUvInfo >= 3 && currentUvInfo <=5) {
-                            spanUvEl.className = ("p-2 bg-warning")
+                            uvSpanEl.className = ("p-2 bg-warning")
                         } else {
-                            spanUvEl.className = ("p-2 bg-danger")                            
+                            uvSpanEl.className = ("p-2 bg-danger")                            
                         }
-
-
-                        //append items 
-                        currentWeatherCityEl.appendChild(currentCityEl);
-                        currentWeatherCityEl.appendChild(iconImgEl);
-
-                        currentWeatherInfoEl.appendChild(tempEl);
-                        currentWeatherInfoEl.appendChild(windEl);
-                        currentWeatherInfoEl.appendChild(humidEl);
-                        currentWeatherInfoEl.appendChild(uvEl);
 
 
                     
